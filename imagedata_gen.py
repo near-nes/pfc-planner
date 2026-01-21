9b70487aa18bc39a819a781711b8fc9a8e9c0fa5
Paul Haider <paul.haider@unibe.ch>
2026-01-21 17:55:18 +0100

    WIP: generalize to variable starting angles

--- imagedata_gen.py
+++ imagedata_gen.py
@@ -19,27 +19,28 @@ from complete_control.config.plant_config import PlantConfig
 from complete_control.plant.robotic_plant import RoboticPlant
 
 if __name__ == "__main__":
-    joint_angle = 90
-    for target in range(0, 151, 10):
-        for color, color_label in zip(
-            (TargetColor.BLUE_LEFT, TargetColor.RED_RIGHT),
-            ("blue", "red"),
-        ):
-            image_path = f"./data/start_{target}_{color_label}.bmp"
-            run_paths = RunPaths.from_run_id(
-                datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-            )
-            config = PlantConfig.from_runpaths(
-                run_paths,
-                simulation=SimulationParams(
-                    oracle=OracleData(
-                        init_joint_angle=joint_angle,
-                        tgt_joint_angle=target,
-                        target_color=color,
-                    )
-                ),
-            )
-            plant = RoboticPlant(config, pybullet)
-            plant._capture_state_and_save(image_path)
-            plant.p.resetSimulation()
-            plant.p.disconnect()
+    for initial in range(0, 151, 10):
+        for target in range(0, 151, 10):
+            for color, color_label in zip(
+                (TargetColor.BLUE_LEFT, TargetColor.RED_RIGHT),
+                ("blue", "red"),
+            ):
+                image_path = f"./data/{initial}_{target}_{color_label}.bmp"
+                run_paths = RunPaths.from_run_id(
+                    datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+                )
+                config = PlantConfig.from_runpaths(
+                    run_paths,
+                    parent_id="",
+                    simulation=SimulationParams(
+                        oracle=OracleData(
+                            init_joint_angle=initial,
+                            tgt_joint_angle=target,
+                            target_color=color,
+                        )
+                    ),
+                )
+                plant = RoboticPlant(config, pybullet)
+                plant._capture_state_and_save(image_path)
+                plant.p.resetSimulation()
+                plant.p.disconnect()
